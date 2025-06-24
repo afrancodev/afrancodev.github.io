2778a6c544ed8dc8c1d27279b8b615a83d1f1eea
André Franco <afranco96@gmail.com>
2025-06-24 01:09:50 +0100

    add dist folder

--- astro.config.ts
+++ astro.config.ts
@@ -13,6 +13,7 @@ import { spectreDark } from './src/ec-theme';
 const config = defineConfig({
   site: 'https://afrancodev.github.io',
   output: 'static',
+  base: '/',
   integrations: [
     expressiveCode({
       themes: [spectreDark],
