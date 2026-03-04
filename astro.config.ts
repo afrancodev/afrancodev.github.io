a6661e5df57a772865968c7c54db0293dfb1fe1c
André Franco <ajcfranco96@gmail.com>
2026-03-04 22:56:26 +0000

    Initial Commit

---
+++ astro.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "astro/config";
+
+import expressiveCode from "astro-expressive-code";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import spectre from "./package/src";
+import node from "@astrojs/node";
+
+import { spectreDark } from "./src/ec-theme";
+
+export default defineConfig({
+  site: "https://afrancodev.github.io",
+  integrations: [
+    expressiveCode({ themes: [spectreDark] }),
+    mdx(),
+    sitemap(),
+    spectre({
+      name: "André Franco",
+      openGraph: {
+        home: {
+          title: "Home | André Franco",
+          description: "Welcome to my Portfolio!",
+        },
+        blog: {
+          title: "Blog | André Franco",
+          description: "Welcome to my blog!",
+        },
+        projects: {
+          title: "Projects | André Franco",
+        },
+      },
+    }),
+  ],
+
+  adapter: node({
+    mode: "standalone",
+  }),
+});
