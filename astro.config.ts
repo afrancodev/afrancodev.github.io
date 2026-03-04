482349f97da4b2dc05cff063bb5f95abdc823d7e
André Franco <ajcfranco96@gmail.com>
2026-03-04 23:05:17 +0000

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import spectre from "./package/src";
-import node from "@astrojs/node";
+// import node from "@astrojs/node"; <--- Remove or comment this out
 
 import { spectreDark } from "./src/ec-theme";
 
 export default defineConfig({
   site: "https://afrancodev.github.io",
+  // base: '/your-repo-name', // ADD THIS if your URL has a subfolder/repo name
   integrations: [
     expressiveCode({ themes: [spectreDark] }),
     mdx(),
@@ -31,8 +31,4 @@ export default defineConfig({
       },
     }),
   ],
-
-  adapter: node({
-    mode: "standalone",
-  }),
 });
